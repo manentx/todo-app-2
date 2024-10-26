be563f0578164d36c96bfff120248747493cc4a5
Cavallo StrateX <cavallostratex@gmail.com>
2024-10-26 19:06:16 +0200

    Feedback on ContactUs
    Aggiunta di feedback sul contenuto di ContactUs, vedi commenti

--- src/screens/ContactUs.tsx
+++ src/screens/ContactUs.tsx
@@ -4,13 +4,27 @@ import Input from "../components/custom/Input";
 import { useState } from "react";
 import Button from "../components/custom/Button";
 
+/* Possibile implementazione: utilizzare SendGrid per inviare effettivamente una mail
+  https://www.twilio.com/docs/sendgrid
+
+  Ad esempio si potrebbe trasformare il form nel tipico form di inserimento contatti (mail, nome e cognome) e far si che, quando
+  il pulsante 'Fissa una call' viene premuto, il sistema invii in automatico una mail all'amministratore (quindi la tua mail) che notifichi il fatto che 
+  è avvenuta una nuova richiesta di call. 
+
+  Cioè l'utente compila il form (per fissare un incontro) e tu ricevi una mail (inviata programmaticamente)
+  del tipo: 'Un nuovo utente ha richiesto di fissare una call. L'utente si chiama Mario Rossi e ha mail mariorossi@gmail.com
+
+  Inoltre, tramite un selettore, si può anche indicare la tipologia di richiesta che l'utente vuole effettuare. Ad esempio 'Richiesta informazioni', 'Problema tecnico', 
+  'Appuntamento conoscitivo'. 
+*/
 function ContactUs() {
   const [message, setMessage] = useState<string>("");
   const [messageSent, setMessageSent] = useState<boolean>(false);
+  
   /*const messageSpedito = () => {
     <div className="text-cyan-600">Messaggio Spedito!</div>
   }*/
-
+    // Feedback: di solito le funzioni che gestiscono degli eventi iniziano con 'on...'. Potresti trasformarla in un 'onSendMessageClick' o simili
     const messaggeSentToMe = () => {
       setMessageSent(true);
     }
@@ -25,6 +39,10 @@ function ContactUs() {
          <Input type="text" className="" value={message} setValue={setMessage} placeholder="Inserisci il tuo messaggio qui..."/>
          <Button disabled={message.trim().length >= 4 ? false : true} title="Invia" onClick={messaggeSentToMe}></Button>
          {
+             // FEEDBACK: Il controllo può essere effettuato togliendo il triplo-uguale
+             // FEEDBACK: Il div che visualizza il messaggio può essere raccolto in un componente a parte, rendendo il codice più leggibile. 
+             // Il componente potrebbe chiamarsi MessageSentBox
+             // Ovviamente la condizione dovrebbe cambiare, mostrando il componente solo se messageSent è true
             (messageSent === true) ? 
             <div className="text-green-500">Messaggio spedito!</div> : 
             null
